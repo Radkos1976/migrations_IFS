59d51f88874c2788bb6611e7897fb780f91c853e
Radomir Kosobucki <radkos76@gmail.com>
2020-09-17 14:49:38 +0200

    Dodaj pliki projektów.

---
+++ Common/Interfaces.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Interfaces
+    {
+        /// <summary>
+        /// Interface For info of subprogram
+        /// </summary>
+        public interface IPluginInfo
+        {
+            string DisplayName { get; }
+            string Description { get; }
+            string Version { get; }
+        }
+        /// <summary>
+        /// Interface of Loger with metods
+        /// </summary>
+        public interface IDB_Loger
+        {
+            void Log(string txt);
+            void Srv_start();
+            void Srv_stop();
+            DateTime Serw_run { get; }
+        }
+        /// <summary>
+        /// Metods for main of plugins
+        /// </summary>
+        public interface IRunnable
+        {
+            void Run();
+        }
+        /// <summary>
+        /// Metods for Database
+        /// </summary>
+        public interface IDBoperations<T>
+        {
+            Task<List<T>> Get_Ora();
+        }
+    }
+}
